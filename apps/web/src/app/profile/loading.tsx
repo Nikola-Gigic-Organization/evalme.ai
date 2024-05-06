8d0cd031e832070b6cd4bc5a08bf9e4c4b65c9ef
Nikola Gigic <nik.gigic@gmail.com>
2024-05-06 10:05:33 +0200

    feat: profile loading page

--- apps/web/src/app/profile/loading.tsx
+++ apps/web/src/app/profile/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="h-app flex justify-center p-8">
-      <div className="w-lg mt-10 flex flex-col gap-y-8 sm:mt-0">
+      <div className="bg-dotted-spacing-4 bg-dotted-gray-500 w-lg mt-10 flex h-fit flex-col gap-y-8 p-8 sm:mt-0">
         <div className="relative h-40 w-full sm:h-28 sm:pt-0">
           <div className="absolute bottom-2 left-2 z-10 flex h-full w-full flex-col justify-between border border-black bg-white p-4">
             <span className="text-2xl font-bold">Details</span>
